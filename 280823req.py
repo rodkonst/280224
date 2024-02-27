0510fae4e12308153fc985b29ae43d817266776d
rodkonst <rodkonst@gmail.com>
2024-02-28 00:02:18 +0300

    initial commit

---
+++ 280823req.py
@@ -0,0 +1,12 @@
+import requests
+import pprint
+# params={"q":"aert"}
+# response=requests.get("https://www.google.com/search",params=params)
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+response = requests.post('https://httpbin.org/post', json={'key':'value'})
+json_response = response.json()
+pprint.pprint(json_response)
+print(json_response['data'])
+print(json_response['headers']['Content-Type'])
